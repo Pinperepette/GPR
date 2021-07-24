1ab25b4b71d9f7bdc71b048fc545c492adbb2ea9
Pinperepette <pinperepette@gmail.com>
2021-07-24 13:03:04 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys, zlib, PIL.Image, base45, cbor2, pyzbar.pyzbar
-print (cbor2.loads(cbor2.loads(zlib.decompress(base45.b45decode(pyzbar.pyzbar.decode(PIL.Image.open(sys.argv[1]))[0].data.decode().replace("HC1:", "")))).value[2]))
+import sys, zlib, PIL.Image, base45, cbor2, pyzbar.pyzbar,rich
+rich.print (cbor2.loads(cbor2.loads(zlib.decompress(base45.b45decode(pyzbar.pyzbar.decode(PIL.Image.open(sys.argv[1]))[0].data.decode().replace("HC1:", "")))).value[2]))
